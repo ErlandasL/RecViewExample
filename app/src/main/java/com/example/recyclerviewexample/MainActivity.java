726568829fb3908ef5724ea3a6c810d8c680420c
ErlandasL <erlandaslisauskas@gmail.com>
2021-08-20 11:32:47 +0300

    Added: files for RecView

---
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.recyclerviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView contactsRecView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactsRecView = findViewById(R.id.contactsRecView);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        contacts.add(new Contact("Name1", "name1@example.com", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png"));
+        contacts.add(new Contact("Name2", "name2@example.com", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png"));
+        contacts.add(new Contact("Name3", "name3@example.com", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png"));
+
+        ContactsRecViewAdapter adapter = new ContactsRecViewAdapter(this);
+        adapter.setContacts(contacts);
+
+        contactsRecView.setAdapter(adapter);
+        contactsRecView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+}
